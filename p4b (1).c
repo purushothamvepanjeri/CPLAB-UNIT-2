516d8298e144dd8f8881ebbcb0ac882c9530a0b8
purushothamvepanjeri <purushothamvepanjeri@gmail.com>
2025-11-19 08:26:18 +0530

    Add files via upload

---
+++ p4b (1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5] ={1,2,3,4,5,};
+    int *ptr =arr;
+    printf("array element using pointer:\n");
+    for (int i=0; i<5; i++)
+    {
+        printf("%d\n",*(ptr +i));
+    }
+    return 0;
+
+}
